806622dcceed0eeee5cd871bebdbe6c99e765f96
Aspect <raphaelyim@gmail.com>
2025-02-23 03:14:57 -0700

    Added a define for flexibility

--- include/stack.h
+++ include/stack.h
@@ -5,17 +5,20 @@
 #ifndef STACK_H
 #define STACH_H
 
+#define STACK_CAPACITY 7
+
 // Defining Stack
 typedef struct Stack
 {
    int items[7];
-   int top;
+   int size;
 } Stack;
 
 void stack_push(Stack* stack);
 void stack_pop(Stack* stack);
 int stack_top(Stack* stack);
-int stack_is_empty(Stack* stack);
-Stack* create_stack();
+int is_stack_empty(Stack* stack);
+int is_stack_full(Stack* stack);
+void create_stack(Stack* stack);
 
 #endif
