adce24c3a2d41b26c44adaddc54b25196206cbc0
Raphael Yim <raphaelyim@gmail.com>
2025-02-24 12:08:03 -0700

    General Fixes, Motor Control Test

--- src/main/main.c
+++ src/main/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "linked_list.h"
-
+#include "motor_init.h"
+#include "motor_control.h"
 
 int main() {
 
+    LinkedListNode* head = create_linked_list(create_servo_motor(0));
+    for (int i = 1; i <= 2; i++) {
+        append_to_linked_list(&head, create_servo_motor(i));
+    }
 
+    move_motor(&head,142);
+    scanf("");
     return 1;
 }
