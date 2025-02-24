a05b59e63acd903db29a6b8979cf3e06c0d6b8ec
Aspect <raphaelyim@gmail.com>
2025-02-23 21:04:15 -0700

    Motor Control Implementation

---
+++ src/core/motor_control.c
@@ -0,0 +1,24 @@
+// Raphael
+// motor_control.c
+// Implementation of functions outlined in motor_control.h
+
+#include "motor_control.h"
+#include <phidget22.h>
+
+// ndeg is between 2 and 4 digits long.
+// (n-1) is the motor to move
+// deg is the degree to move it too
+void move_motor(LinkedListNode* head, int ndeg) {
+
+    int n = ndeg;
+    int digit = 0;
+    while (n >= 10) {
+        n /= 10;
+        digit ++;
+    }
+    digit += 1;
+    PhidgetRCServoHandle* motor = get_linked_list_value(n-1,head)->value;
+
+    int deg = ndeg - (n*(digit*10));
+
+}
